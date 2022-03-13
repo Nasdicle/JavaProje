d2e00c50bdeb12dd3d01b875158c52b77e38611f
Nasdicle <78178470+Nasdicle@users.noreply.github.com>
2022-03-13 21:44:55 +0300

    Create VucutİndeksiHesaplama.java

---
+++ VucutİndeksiHesaplama.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+public static void main(String[] args)
+{
+  double kilo,boy,indeks
+
+  
+  Scanner input = new Scanner();
+  System.out.printIn("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+  boy = input.nextDouble;
+  
+  System.out.printIn("Lütfen kilonuzu giriniz: ");
+  kilo = input.nextDouble;
+  
+  indeks = kilo / (boy * boy);
+  System.out.printIn("Vücut Kitle İndeksiniz: " + indeks);
+}
+}
